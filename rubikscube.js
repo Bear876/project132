5685dd5e7d6f653c6051091673715ee60a83388c
Bear876 <124530493+Bear876@users.noreply.github.com>
2024-09-23 20:39:09 -0500

    Add files via upload

---
+++ rubikscube.js
@@ -0,0 +1,27 @@
+rubikscube="";
+status ="";
+
+function preload(){
+    rubikscube = loadImage('rubikscube.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(rubikscube,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    } 
+    console.log(results);
+    objects=results;
+}
